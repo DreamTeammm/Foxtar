16489a1454e1a916fce00b7e9f763bfc0af48f40
Tanya Vergun <gaudenko.tanya@gmail.com>
2018-11-24 00:00:17 +0200

    added popup

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,12 @@ $(function(){
 		$('.menu').toggleClass('open');
 	});
 });
+
+$(function(){
+  $('button').click(function(){
+    $('.popup,.popup_overlay').fadeIn(400); //показываем всплывающее окно
+  });
+  $('.closer,.popup_overlay').click(function(){
+    $('.popup,.popup_overlay').fadeOut(400); //скрываем всплывающее окно
+  });
+});
